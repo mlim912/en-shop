7ce2d5c054b45bc522b6a5b4a770fff86ecbcd25
Molika Lim <limmolika99@gmail.com>
2022-10-27 13:02:13 +1300

    Updates

---
+++ precache-manifest.1b179df1dec1ac2bb0bff399c1729917.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305546df2a7fec2f8bdbaafe8d853599",
+    "url": "/en-shop/index.html"
+  },
+  {
+    "revision": "2b1ec8b840854dccc726",
+    "url": "/en-shop/static/css/main.2302fb0f.chunk.css"
+  },
+  {
+    "revision": "201ba8db28e7fa9aa8ef",
+    "url": "/en-shop/static/js/2.61d45903.chunk.js"
+  },
+  {
+    "revision": "0b2597c017b8be349b21b9be4f5ae4e3",
+    "url": "/en-shop/static/js/2.61d45903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1ec8b840854dccc726",
+    "url": "/en-shop/static/js/main.e3cfd9f0.chunk.js"
+  },
+  {
+    "revision": "0bb085eb22a5fda9b58b",
+    "url": "/en-shop/static/js/runtime-main.8d81ad5d.js"
+  },
+  {
+    "revision": "7e826a7db0c2d3c48604791170a22089",
+    "url": "/en-shop/static/media/logo.7e826a7d.png"
+  },
+  {
+    "revision": "19190d053a8b13a3d9f528c9f7628817",
+    "url": "/en-shop/static/media/shopMens.19190d05.jpg"
+  },
+  {
+    "revision": "c9c60ae5dd1c192f2617996f1b693582",
+    "url": "/en-shop/static/media/shopWomens.c9c60ae5.jpg"
+  }
+]);
